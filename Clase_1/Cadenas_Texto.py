f21d1dd3a68577d45c17a61b69b312f1f5ddb62b
camilo franco <camilofinanzas@gmail.com>
2023-08-09 11:15:34 -0500

    clase 1 - actualizacion archivos

---
+++ Clase_1/Cadenas_Texto.py
@@ -0,0 +1,28 @@
+# Ejercicio No 1
+
+
+cadenamultiple = '''
+    Por Favor Ingresa una Frase este programa
+    cuenta el numero de palabras que contiene
+    una frase
+'''
+
+print(cadenamultiple)
+
+frase = input("Ingresa una frase : ")
+
+frase_sin_espacios = frase.replace(" ","")
+
+print(len(frase))
+print(len(frase_sin_espacios))
+
+
+
+numeroletrasfrase = len(frase) - len(frase_sin_espacios)
+
+print("EL Numero de palabreas en la frase es :",numeroletrasfrase +1)
+
+favorite_lenguaje = '    python    '
+valor= favorite_lenguaje.lstrip()
+print(valor)
+print(favorite_lenguaje)
